f21bff9f9f71707320fea5661d3869b435c9c718
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-29 11:30:22 +0530

    Master Branch Welcome

---
+++ src/main/java/com/training/AddressBook/AddressBook.java
@@ -0,0 +1,5 @@
+package com.training.AddressBook;
+
+public class AddressBook {
+
+}
---
+++ src/main/java/com/training/main/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.training.main;
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Address Book Program !");
+	}
+
+}
