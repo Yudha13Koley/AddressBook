26d05d9498c70359c6602fcb69655132da5f8eba
Yudha13Koley <yudhajit1998@gmail.com>
2020-09-29 12:21:32 +0530

    UC2 done

--- src/main/java/com/training/main/AddressBookMain.java
+++ src/main/java/com/training/main/AddressBookMain.java
@@ -7,6 +7,12 @@ import com.training.AddressBook.AddressBook;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Address Book Program !");
+		AddressBook A1=new AddressBook();
+		Scanner sc=new Scanner(System.in);
+		A1.addConatct(sc);
+		A1.addConatct(sc);
+		A1.addConatct(sc);
+		A1.printContacts();
 	}
 
 }
